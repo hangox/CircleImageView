62e5d5813b3997beb76c2b8dd477f5b04e050fa2
hangox <liang.hanguang93@gmail.com>
2015-03-12 09:56:08 +0800

    add 新的demo

--- app/src/main/java/org/hangox/circleimageview/MainActivity.java
+++ app/src/main/java/org/hangox/circleimageview/MainActivity.java
@@ -1,11 +1,21 @@
 package org.hangox.circleimageview;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
-import com.lidroid.xutils.view.annotation.ViewInject;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +23,11 @@ import java.util.List;
 import auggie.library.displayers.CircleBitmapDisplayer;
 import auggie.library.displayers.ScaleRoundedBitmapDisplayer;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity {
-    @ViewInject(R.id.imageList)
+    @InjectView(R.id.imageList)
     GridView gViewImage;
 
     List<DisplayImageOptions> imageOptionsList = new ArrayList<>(10);
@@ -31,6 +42,7 @@ public class MainActivity extends ActionBarActivity {
 
     private void setUpOption(){
         DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(new ColorDrawable(Color.BLUE))
                 .cacheInMemory(true)
                 .cacheOnDisk(true)
                 .build();
@@ -38,9 +50,58 @@ public class MainActivity extends ActionBarActivity {
                 CircleBitmapDisplayer()).build());
         imageOptionsList.add(new DisplayImageOptions.Builder().cloneFrom(options).displayer(new
                 ScaleRoundedBitmapDisplayer(8)).build());
+        gViewImage.setAdapter(new ItemAdapter());
 
     }
 
+
+    public class ItemAdapter extends BaseAdapter{
+        public static final String IMAGE_URL = "http://c.hiphotos.baidu.com/image/pic/item/7acb0a46f21fbe09f9d1479869600c338644adab.jpg";
+        @Override
+        public int getCount() {
+            return imageOptionsList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                convertView= View.inflate(MainActivity.this,R.layout.adapter_image_show,null);
+                convertView.setTag(new ViewHolder(convertView));
+            }
+            ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+            DisplayImageOptions options = imageOptionsList.get(position);
+            ImageLoader.getInstance().displayImage(IMAGE_URL,viewHolder.iViewShow,options,new SimpleImageLoadingListener(){
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    super.onLoadingComplete(imageUri, view, loadedImage);
+                    Log.i("imageloader","completed");
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    Log.i("imageloader", "fail");
+                }
+            });
+            return convertView;
+        }
+
+        public class ViewHolder {
+            public ImageView iViewShow;
+            public ViewHolder(View parent){
+                iViewShow  = (ImageView) parent.findViewById(R.id.imageView);
+            }
+        }
+    }
 
 
 
